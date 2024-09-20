cec266b3bae65639d09bf2b14610645a85bb3760
Oumar Yanni <oumaryanni@gmail.com>
2024-09-20 12:51:13 +0000

    Display with if and variables

---
+++ src/components/Article.jsx
@@ -0,0 +1,28 @@
+function Article() {
+  return (
+    <div className="card p-20">
+      <h2 className="mb-10">Titre de l'article</h2>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut earum
+        quibusdam voluptatibus, veritatis aperiam ducimus. Minima repellendus
+        quam aliquid amet expedita ratione soluta cupiditate. Cupiditate, rerum?
+        Ad vero fuga iusto? Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. A voluptatem at quia enim velit eius, quibusdam corrupti inventore
+        quasi? Tempora optio delectus ratione nobis maiores harum excepturi
+        soluta hic nesciunt. Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Vero repellendus est deleniti at, numquam possimus quaerat magnam
+        illum maxime aliquam commodi laborum eveniet corporis vitae, sunt eum
+        incidunt, molestias atque. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Saepe minima neque quia molestiae commodi velit
+        tenetur ratione sint aspernatur sunt, odio cumque maiores, autem ipsum
+        placeat sapiente aliquid possimus recusandae? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Perspiciatis pariatur officiis quam modi.
+        Quam necessitatibus temporibus velit quia, minima iure accusamus nisi,
+        sit, magnam labore vel nam a vero qui?
+      </p>
+    </div>
+  );
+}
+
+export default Article;

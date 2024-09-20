a6e3a78f6b455ae877716716ad76ce4292de7abd
Oumar Yanni <oumaryanni@gmail.com>
2024-09-20 20:29:11 +0000

    Displaying lists with map

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,16 +1,21 @@
 import Article from "./Article";
 
-function Articles({ displayArticle }) {
-  let content = <div>Aucun article</div>;
-
-  if (displayArticle) {
-    content = <Article />;
-  }
+function Articles({}) {
+  const articles = [
+    { title: "Un tire 1", content: "Contenu 1" },
+    { title: "Un titre 2", content: "Contenu 2" },
+    { title: "Un titre 3", content: "Contenu 3" },
+  ];
   return (
     <div style={{ width: "700px" }}>
       <h1 className="mb-20">Liste des articles</h1>
 
-      {content}
+      {articles.map((article) => (
+        <div className="mb-20">
+          <h2>{article.title}</h2>
+          <p>{article.content}</p>
+        </div>
+      ))}
     </div>
   );
 }

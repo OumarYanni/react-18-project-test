5e87e98af2b1779e9212576357757d33c92afcf1
Oumar Yanni <oumaryanni@gmail.com>
2024-09-20 21:54:59 +0000

    Using filter, keys and fragments

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,20 +1,23 @@
+import { Fragment } from "react";
 import Article from "./Article";
 
 function Articles({}) {
   const articles = [
-    { title: "Un tire 1", content: "Contenu 1" },
-    { title: "Un titre 2", content: "Contenu 2" },
-    { title: "Un titre 3", content: "Contenu 3" },
+    { title: "Un titre 1", content: "Contenu 1", id: 1 },
+    { title: "Un titre 2", content: "Contenu 2", id: 2 },
+    { title: "Un titre 3", content: "Contenu 3", id: 3 },
   ];
   return (
     <div style={{ width: "700px" }}>
       <h1 className="mb-20">Liste des articles</h1>
 
       {articles.map((article) => (
-        <div className="mb-20">
-          <h2>{article.title}</h2>
-          <p>{article.content}</p>
-        </div>
+        <Fragment key={article.id}>
+          <div className="mb-20 card p-20">
+            <h2 className="mb-10">{article.title}</h2>
+            <p>{article.content}</p>
+          </div>
+        </Fragment>
       ))}
     </div>
   );
